69494c7ff17bd639b2b8f2f2bddb35fec31015db
DevSlowz <msvieira021@gmail.com>
2024-05-20 09:43:09 -0400

    Add Stock class for containing info about individual stocks

---
+++ stocks.py
@@ -0,0 +1,18 @@
+class Stock:
+    def __init__(self, stock_symbol, stock_price, price_to_earning, earning_per_share,
+                 dividend_yield, market_cap, doe_ratio):
+        self.stock_symbol = stock_symbol
+        self.stock_price = stock_price
+        self.price_to_earning = price_to_earning
+        self.earning_per_share = earning_per_share
+        self.dividend_yield = dividend_yield
+        self.market_cap = market_cap
+        self.doe_ratio = doe_ratio
+        self.stock_list = []
+
+
+
+    def collect_stock_stats(self):
+        pass
+
+
