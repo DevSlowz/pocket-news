b70a2166cbe14ce73d8758613868fa9b2fbcb2c4
DevSlowz <msvieira021@gmail.com>
2024-05-20 09:56:09 -0400

    Add options for user input

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ from selenium.webdriver.common.keys import Keys
 import time
 def main ():
 
+    main_menu_option = ""
+
+    # Welcome Message
+    print("Welcom to Pocket News Your Handy Partner For Keeping Track Of Stocks.")
+
+    # Determin what user wants to do
+    # 1. View active stats on current list of stocks
+    # 2. View list of active stocks
+    # 3. Remove stock Symbol
+    # 4. Add stock symbol
+
+    main_menu_option = input('''Please enter an option: \n 
+1. View active stats on current list of stocks \n 
+2. View list of active stocks \n 
+3. Remove stock Symbol \n 
+4. Add stock symbol \n 
+Option: ''')
+
     # Navigate to Yahoo Finance
     # Locate the search bar
     # Search for the user specified stock symbol - We will need to determine if the symbol is valid somehow
