ea17eb62cc3d88cecadf3d3c85b32580f85d269f
DevSlowz <msvieira021@gmail.com>
2024-05-20 10:20:56 -0400

    Add user options to main and test option 4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from stocks import Stock
 import time
 def main ():
 
@@ -21,5 +22,13 @@ def main ():
 3. Remove stock Symbol \n 
 4. Add stock symbol \n 
 Option: ''')
+    
+    if main_menu_option == "4" :
+        stock = Stock("AAPL", 0, 0, 0, 0, 0, 0)  # Initialize Stock instance
+        add_symbol = ""
+        add_symbol = input("Please enter valid symbol you would like to add: ")
+        stock.add_stock(add_symbol)
+
+
 
 main()
